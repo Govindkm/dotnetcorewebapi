3b892e1ed09e720440eb228c8a3ea6225fa2db28
Govind Mishra <35376412+Govindkm@users.noreply.github.com>
2025-01-26 11:33:10 +0000

    initial commit

---
+++ UserManagementAPI/Program.cs
@@ -0,0 +1,31 @@
+using Microsoft.EntityFrameworkCore;
+using UserManagementAPI.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Configure in-memory database for simplicity.
+builder.Services.AddDbContext<UserContext>(options =>
+    options.UseInMemoryDatabase("UserDB"));
+
+// Build the app.
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
